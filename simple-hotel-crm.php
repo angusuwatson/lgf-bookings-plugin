47cf16ee9817b1345235b8c75aa0a71c6071e437
Angus <angus@lgf.homelab.com>
2026-07-09 14:28:18 +0200

    chore: bump v1.9.3.11 → v1.9.3.12

--- simple-hotel-crm.php
+++ simple-hotel-crm.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: LGF Bookings
  * Description: A simple WordPress-native hotel CRM with calendar and booking management tools
- * Version: 1.9.3.11
+ * Version: 1.9.3.12
  * Update URI: https://github.com/angusuwatson/lgf-bookings-plugin
  * Author: Angus Watson
  * Text Domain: simple-hotel-crm
