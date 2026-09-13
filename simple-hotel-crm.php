5b63e77cb73b03e14c7eb1203291247197be997e
Angus <angus@lgf.homelab.com>
2026-09-13 15:21:43 +0200

    style(pwa): combine Discount and Value into one cell in Add/Edit popups

--- simple-hotel-crm.php
+++ simple-hotel-crm.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: LGF Bookings
  * Description: A simple WordPress-native hotel CRM with calendar and booking management tools
- * Version: 1.9.4.62
+ * Version: 1.9.4.63
  * Update URI: https://github.com/angusuwatson/lgf-bookings-plugin
  * Author: Angus Watson
  * Text Domain: simple-hotel-crm
